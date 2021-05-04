132e87c6838a09094f45109b74ab32cb890ae52d
stephgeorge <stephanie.george22@gmail.com>
2021-05-04 01:06:40 -0400

    prompt for the length of password and validate input

--- assets/css/js/script.js
+++ assets/css/js/script.js
@@ -1,5 +1,19 @@
 // Assignment code here
 
+//when user clicks the button, they are presented with a series of prompts
+var generatePassword = function() {
+  // password length prompt
+  var generate = window.prompt('Choose a password length between 8 to 128 characters.');
+
+  // ensure that user selects integer between 8 and 128 
+  if (generate < 8 || generate > 128 || generate !== Number.isInteger) {
+      window.alert("Choose a length between 8 and 128 characters.");
+      return generatePassword();
+
+    } else {
+    confirmNumber = confirm("Do you want to include numbers?");
+  }
+};
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
