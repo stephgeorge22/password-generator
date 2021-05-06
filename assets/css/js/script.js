e4cdb63276845db853cd44083fbca81fc5620537
stephgeorge <stephanie.george22@gmail.com>
2021-05-06 01:19:11 -0400

    create array to hold prompt answers

--- assets/css/js/script.js
+++ assets/css/js/script.js
@@ -1,9 +1,12 @@
 // Assignment code here
 
-
+//array to hold prompt answers
+var picked =[];
+//array to hold password
+var result = [];
 
 //when user clicks the button, they are presented with a series of prompts
-var generatePassword = function() {
+ var generatePassword = function() {
   // password length prompt
   var length = parseInt (window.prompt('Choose a password length between 8 to 128 characters.'));
 
@@ -13,30 +16,52 @@ var generatePassword = function() {
       return generatePassword();
 
     } else {
-    Lowercase = confirm("Should your password include lowercase letters?");
-    Uppercase = confirm("Should your password include uppercase letters?");
-    Number = confirm("Should your password include numeric values?");
-    SpecialCharacter = confirm("Should your password include special characters?");
+    pickLowercase = confirm("Should your password include lowercase letters?");
+    pickUppercase = confirm("Should your password include uppercase letters?");
+    pickNumber = confirm("Should your password include numeric values?");
+    pickSpecialCharacter = confirm("Should your password include special characters?");
   }
 
-  // functions to generate random options utilizing Charcode
-  function randomLowercase(){
-    return String.fromCharCode(Math.floor(Math.random() *26) + 41);
-  }
+  // add all prompt responses in else to an array
+    picked.push(pickLowercase);
+    picked.push(pickUppercase);
+    picked.push(pickNumber);
+    picked.push(pickSpecialCharacter);
 
-  function randomUppercase(){
-    return String.fromCharCode(Math.floor(Math.random() *26) + 61);
-  }
+    console.log(picked);
+  
+  // create for loop with all 4 functions for array 
 
-  function randomNumber(){
-    return String.fromCharCode(Math.floor(Math.random() *10) + 30);
-  }
+  // only run functions that array has stored 
 
-  function randomSpecialcharacter(){
-    return String.fromCharCode(Math.floor(Math.random() *15) + 21);
+  
+
+  for (i=0; i<length; i++ ) {
+    lowerCase = String.fromCharCode(Math.floor(Math.random() *26) + 97);
+
+    result.push(lowerCase);
   }
+  return result.join('');
+
 
 };
+
+ // functions to generate random options utilizing Charcode (https://net-comber.com/charset.html)
+ function randomLowercase(){
+  return String.fromCharCode(Math.floor(Math.random() *26) + 97);
+}
+
+function randomUppercase(){
+  return String.fromCharCode(Math.floor(Math.random() *26) + 61);
+}
+
+function randomNumber(){
+  return String.fromCharCode(Math.floor(Math.random() *10) + 30);
+}
+
+function randomSpecialcharacter(){
+  return String.fromCharCode(Math.floor(Math.random() *15) + 21);
+}
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
