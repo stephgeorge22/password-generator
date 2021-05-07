67d9bd551c13bf61053bc6637027bdcf4b0dcb32
stephgeorge <stephanie.george22@gmail.com>
2021-05-07 19:40:12 -0400

    password contains at least one of every selected character type

--- assets/css/js/script.js
+++ assets/css/js/script.js
@@ -37,6 +37,43 @@ var specialCharacterResult = picked[3];
 
     console.log(picked);
 
+  //ENSURE THAT ONE OF EACH CHOSEN CHARACTER TYPE IS INCLUDED
+  // add one character of each true prompt to result array 
+  if (pickLowercase === true) {
+    lowerCase = String.fromCharCode(Math.floor(Math.random() *26) + 97);
+    result.push(lowerCase);
+  };
+
+  if (pickLowercase === true) {
+    upperCase = String.fromCharCode(Math.floor(Math.random() *26) + 65);
+    result.push(upperCase);
+  };
+
+  if (pickNumber === true) {
+    number = String.fromCharCode(Math.floor(Math.random() *26) + 48);
+    result.push(number);
+  };
+
+  if (pickSpecialCharacter === true) {
+    special = String.fromCharCode(Math.floor(Math.random() *26) + 33);
+    result.push(special);
+  };
+
+  console.log(result);
+
+  // calculate number of true values in picked array
+  var count = picked.filter(Boolean).length;
+
+  console.log(count);
+
+  //subtract number of true values from for loop length 
+ 
+  // ONLY SELECTED CHARACTERS ARE CHOSEN
+  // identify positions of true values in picked array
+  // take true value position number and add into new array
+  // change random generating function to pick number from that array 
+
+
   // ensure user chooses at least one character type
   if (picked[0] === false && picked[1] === false && picked[2] === false && picked[3] === false) {
     window.alert("You must select at least one character type")
@@ -46,50 +83,96 @@ var specialCharacterResult = picked[3];
   
   // create for loop with all 4 functions for array 
   // functions to generate random options utilizing Charcode (https://net-comber.com/charset.html)
-
   // only run functions that array has stored as true
+  for (i=0; i < (length - count); i++ ) {
+    //random select number between 0 to 3 
 
-  for (i=0; i<length; i++ ) {
+    var random = (Math.floor(Math.random() * 4) + 0) ;
+    console.log(random);
+  
+      // if x is selected and picked[x] = false then rerun random selection
+      // if (picked[random] === false) {
+      //   return random();
+      // } 
+      // else {
+        switch (random) {
+          case 0:
+            lowerCase = String.fromCharCode(Math.floor(Math.random() *26) + 97);
+            result.push(lowerCase);
+            console.log(lowerCase);
+            break;
+          
+          case 1:
+            upperCase = String.fromCharCode(Math.floor(Math.random() *26) + 65);
+            result.push(upperCase);
+            console.log(upperCase);
+            break;
 
-  }
-
-  for (i=0; i<length; i++ ) {
-
-    if (picked[0] === true) {
-      // function for lowercase
-      lowerCase = String.fromCharCode(Math.floor(Math.random() *26) + 97);
-      result.push(lowerCase);
-    }
-
-    console.log(result);
-
-    if (picked[1] === true) {
-      //function for uppercase
-      upperCase = String.fromCharCode(Math.floor(Math.random() *26) + 65);
-      result.push(upperCase);
-    }
-    
-    console.log(result);
-
-    if (picked[2] === true) {
-      //function for numbers
-      number = String.fromCharCode(Math.floor(Math.random() *10) + 48);
-      result.push(number);
-    }
-    console.log(result);
-
-    if (picked[3] === true) {
-      //function for special characters
-      specialCharacters = String.fromCharCode(Math.floor(Math.random() *15) + 33);
-      result.push(specialCharacters);
-    }
-
-  }
+          case 2:
+              number = String.fromCharCode(Math.floor(Math.random() *26) + 48);
+              result.push(number);
+              console.log(number);
+              break;
+        
+          case 3:
+            special = String.fromCharCode(Math.floor(Math.random() *26) + 33);
+            result.push(special);
+            console.log(special);
+            break;
+        }
+      // }
+    };
+  // };
   return result.join('');
+}
+  
+  
+    // if x is selected and picked[x] = true
+
+
+    // then run funtion associated ...... 0 = lowerCase , 1 = upperCase , 2 = numbers, 3 = specialCharacters
+  
+    // place inside for loop 
+  
+ 
+
+//   for (i=0; i<length; i++ ) {
+
+//     if (picked[0] === true) {
+//       // function for lowercase
+//       lowerCase = String.fromCharCode(Math.floor(Math.random() *26) + 97);
+//       result.push(lowerCase);
+//     }
+
+    
+
+//     if (picked[1] === true) {
+//       //function for uppercase
+//       upperCase = String.fromCharCode(Math.floor(Math.random() *26) + 65);
+//       result.push(upperCase);
+//     }
+    
+    
+
+//     if (picked[2] === true) {
+//       //function for numbers
+//       number = String.fromCharCode(Math.floor(Math.random() *10) + 48);
+//       result.push(number);
+//     }
+    
+
+//     if (picked[3] === true) {
+//       //function for special characters
+//       specialCharacters = String.fromCharCode(Math.floor(Math.random() *15) + 33);
+//       result.push(specialCharacters);
+//     }
+
+//   }
+//   return result.join('');
   
 
 
-};
+// };
 
  
 
