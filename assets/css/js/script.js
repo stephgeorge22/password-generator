9b15f212e9f3e20ed68e78240feec4e607f563ce
stephgeorge <stephanie.george22@gmail.com>
2021-05-07 23:18:03 -0400

    password randomly generates

--- assets/css/js/script.js
+++ assets/css/js/script.js
@@ -3,17 +3,18 @@
 //array to hold prompt answers
 var picked =[];
 
+//array to hold index value of true prompt answers
+var index = [];
+
 //array to hold password
 var result = [];
 
-// assigning value to each array element to call in for loop
-var lowerResult = picked[0];
-var upperResult = picked[1];
-var numberResult = picked[2];
-var specialCharacterResult = picked[3];
-
 //when user clicks the button, they are presented with a series of prompts
  var generatePassword = function() {
+   picked.length = 0; 
+   index.length = 0; 
+   result.length = 0;
+
   // password length prompt
   var length = parseInt (window.prompt('Choose a password length between 8 to 128 characters.'));
 
@@ -35,8 +36,6 @@ var specialCharacterResult = picked[3];
     picked.push(pickNumber);
     picked.push(pickSpecialCharacter);
 
-    console.log(picked);
-
   //ENSURE THAT ONE OF EACH CHOSEN CHARACTER TYPE IS INCLUDED
   // add one character of each true prompt to result array 
   if (pickLowercase === true) {
@@ -44,137 +43,74 @@ var specialCharacterResult = picked[3];
     result.push(lowerCase);
   };
 
-  if (pickLowercase === true) {
+  if (pickUppercase === true) {
     upperCase = String.fromCharCode(Math.floor(Math.random() *26) + 65);
     result.push(upperCase);
   };
 
   if (pickNumber === true) {
-    number = String.fromCharCode(Math.floor(Math.random() *26) + 48);
+    number = String.fromCharCode(Math.floor(Math.random() *10) + 48);
     result.push(number);
   };
 
   if (pickSpecialCharacter === true) {
-    special = String.fromCharCode(Math.floor(Math.random() *26) + 33);
+    special = String.fromCharCode(Math.floor(Math.random() *15) + 33);
     result.push(special);
   };
-
-  console.log(result);
 
   // calculate number of true values in picked array
   var count = picked.filter(Boolean).length;
 
-  console.log(count);
-
-  //subtract number of true values from for loop length 
+  //subtract number of true values from form loop length 
  
   // ONLY SELECTED CHARACTERS ARE CHOSEN
-  // identify positions of true values in picked array
-  // take true value position number and add into new array
-  // change random generating function to pick number from that array 
-
-
+  // identify positions of true values in picked array and put into index array
+  for (i = 0; i < picked.length ; i++) {
+    if(picked[i] === true) {
+      indexTrue = i ;
+      index.push(indexTrue);
+    };
+  };
+ 
   // ensure user chooses at least one character type
   if (picked[0] === false && picked[1] === false && picked[2] === false && picked[3] === false) {
     window.alert("You must select at least one character type")
     return generatePassword();
   }
-
-  
+    
   // create for loop with all 4 functions for array 
   // functions to generate random options utilizing Charcode (https://net-comber.com/charset.html)
   // only run functions that array has stored as true
   for (i=0; i < (length - count); i++ ) {
-    //random select number between 0 to 3 
+    //random select number from index array  
 
-    var random = (Math.floor(Math.random() * 4) + 0) ;
-    console.log(random);
-  
-      // if x is selected and picked[x] = false then rerun random selection
-      // if (picked[random] === false) {
-      //   return random();
-      // } 
-      // else {
-        switch (random) {
-          case 0:
-            lowerCase = String.fromCharCode(Math.floor(Math.random() *26) + 97);
-            result.push(lowerCase);
-            console.log(lowerCase);
-            break;
-          
-          case 1:
-            upperCase = String.fromCharCode(Math.floor(Math.random() *26) + 65);
-            result.push(upperCase);
-            console.log(upperCase);
-            break;
+    var random ="";
+    random = index[Math.floor(Math.random()* index.length)];
+   
+    switch (random) {
+      case 0:
+        lowerCase = String.fromCharCode(Math.floor(Math.random() *26) + 97);
+        result.push(lowerCase);
+        break;
+      
+      case 1:
+        upperCase = String.fromCharCode(Math.floor(Math.random() *26) + 65);
+        result.push(upperCase);
+        break;
 
-          case 2:
-              number = String.fromCharCode(Math.floor(Math.random() *26) + 48);
-              result.push(number);
-              console.log(number);
-              break;
-        
-          case 3:
-            special = String.fromCharCode(Math.floor(Math.random() *26) + 33);
-            result.push(special);
-            console.log(special);
-            break;
-        }
-      // }
-    };
-  // };
+      case 2:
+          number = String.fromCharCode(Math.floor(Math.random() *10) + 48);
+          result.push(number);
+          break;
+    
+      case 3:
+        special = String.fromCharCode(Math.floor(Math.random() *15) + 33);
+        result.push(special);
+        break;
+    }
+  };
   return result.join('');
-}
-  
-  
-    // if x is selected and picked[x] = true
-
-
-    // then run funtion associated ...... 0 = lowerCase , 1 = upperCase , 2 = numbers, 3 = specialCharacters
-  
-    // place inside for loop 
-  
- 
-
-//   for (i=0; i<length; i++ ) {
-
-//     if (picked[0] === true) {
-//       // function for lowercase
-//       lowerCase = String.fromCharCode(Math.floor(Math.random() *26) + 97);
-//       result.push(lowerCase);
-//     }
-
-    
-
-//     if (picked[1] === true) {
-//       //function for uppercase
-//       upperCase = String.fromCharCode(Math.floor(Math.random() *26) + 65);
-//       result.push(upperCase);
-//     }
-    
-    
-
-//     if (picked[2] === true) {
-//       //function for numbers
-//       number = String.fromCharCode(Math.floor(Math.random() *10) + 48);
-//       result.push(number);
-//     }
-    
-
-//     if (picked[3] === true) {
-//       //function for special characters
-//       specialCharacters = String.fromCharCode(Math.floor(Math.random() *15) + 33);
-//       result.push(specialCharacters);
-//     }
-
-//   }
-//   return result.join('');
-  
-
-
-// };
-
- 
+};
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
